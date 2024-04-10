af2c263983339d1b22c98824ec9ab854e23b4b13
Sara Ferreira <sara.f.araujo12@gmail.com>
2024-04-10 13:06:20 -0300

    adding sql script

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -1,1 +1,27 @@
 CREATE DATABASE users_db;
+
+USE users_db;
+
+CREATE TABLE users (
+	username VARCHAR(50) PRIMARY KEY NOT NULL,
+    password VARCHAR(50) NOT NULL,
+	enabled TINYINT NOT NULL
+);
+
+INSERT INTO users VALUES ("john", "{noop}john123", 1),
+("mary", "{noop}mary123", 1),
+("susan", "{noop}susan123", 1);
+
+CREATE TABLE authorities (
+	username VARCHAR(50) NOT NULL,
+    authority VARCHAR(50) NOT NULL,
+	UNIQUE KEY authorities_idx_1 (username, authority),
+    CONSTRAINT authorities_ibfk_1 FOREIGN KEY (username) REFERENCES users (username)
+);
+
+INSERT INTO authorities VALUES ("john", "ROLE_EMPLOYEE"),
+("mary", "ROLE_EMPLOYEE"),
+("mary", "ROLE_MANAGER"),
+("susan", "ROLE_EMPLOYEE"),
+("susan", "ROLE_MANAGER"),
+("susan", "ROLE_ADMIN");
