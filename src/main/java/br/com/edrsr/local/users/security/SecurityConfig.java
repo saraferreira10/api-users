7b4136dcf9ede1ad72093283c387959bdaf00b8f
Sara Ferreira <sara.f.araujo12@gmail.com>
2024-04-10 13:19:59 -0300

    feat: logging in with database users

--- src/main/java/br/com/edrsr/local/users/security/SecurityConfig.java
+++ src/main/java/br/com/edrsr/local/users/security/SecurityConfig.java
@@ -5,35 +5,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager(){
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}test123")
-                .roles("EMPLOYEE")
-                .build();
-
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}mary123")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
-
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password("{noop}susan123")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(john, mary, susan);
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
@@ -45,7 +28,6 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/users").hasRole("MANAGER")
                         .requestMatchers(HttpMethod.PUT, "/users").hasRole("MANAGER")
                         .requestMatchers(HttpMethod.DELETE, "/users/**").hasRole("ADMIN")
-
         );
 
         httpSecurity.httpBasic(Customizer.withDefaults());
