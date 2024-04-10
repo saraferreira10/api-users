de9b59212de6032737ea71c1c5a8f7bf252489ab
Sara Ferreira <sara.f.araujo12@gmail.com>
2024-04-10 10:57:59 -0300

    feat: adding spring security

--- src/main/java/br/com/edrsr/local/users/controller/DepartmentController.java
+++ src/main/java/br/com/edrsr/local/users/controller/DepartmentController.java
@@ -3,6 +3,7 @@ package br.com.edrsr.local.users.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import br.com.edrsr.local.users.service.DepartmentService;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/departments")
 public class DepartmentController {
--- src/main/java/br/com/edrsr/local/users/controller/UserController.java
+++ src/main/java/br/com/edrsr/local/users/controller/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import br.com.edrsr.local.users.service.UserService;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/users")
 public class UserController {
